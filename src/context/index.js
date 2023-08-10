35e749bab229dde1307c1f23e328550f7caa8628
ankitkumar1995 <ankitknit15@gmail.com>
2023-08-10 08:54:27 +0530

    seting global state

---
+++ src/context/index.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+export const GlobalContext = createContext(null);
+export default function GlobalState({ children }) {
+  return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
+}
